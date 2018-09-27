082063a18244e1b91b9f924fbd676892efb3a5fe
HARRISONS-MK\G1 <harrison_silva_2@hotmail.com>
2018-09-27 17:31:45 +0200

    primer commit

---
+++ RevenueCash.ServicesLibrary/JuegosServices/IJuegoServices.cs
@@ -0,0 +1,21 @@
+﻿using RevenueCash.Models.Juego;
+using RevenueCash.Models.Piezas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevenueCash.ServicesLibrary.JuegosServices
+{
+    public interface IJuegoServices
+    {
+        Game ComenzarNuevoJuego(int nivel);
+
+        Tablero GetTableroDeNivel(int nivel);
+
+        Game DisparaFicha(Game game, Posicion desdeDonde, int indice);
+
+        IList<FichaDisparo> GetFichaDisparo(int size, Posicion desdeDonde);
+    }
+}
---
+++ RevenueCash.ServicesLibrary/JuegosServices/JuegoServices.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RevenueCash.Models.Juego;
+using RevenueCash.Models.Piezas;
+
+namespace RevenueCash.ServicesLibrary.JuegosServices
+{
+    public class JuegoServices : IJuegoServices
+    {
+        public Game ComenzarNuevoJuego(int nivel)
+        {
+            Game nuevoJuego = Game.GenerateNewGame(GameDifficulty.Easy);
+            nuevoJuego.Board = this.GetTableroDeNivel(1);
+            return nuevoJuego;
+        }
+
+        public Game DisparaFicha(Game game, Posicion desdeDonde, int indice)
+        {
+            if(desdeDonde == Posicion.Arriba)
+            {
+                int indiceTope = -1;
+                for (int fila = 0; fila<= game.Board.Size; fila++)
+                {
+                    if (game.Board.Celdas[fila, indice].Ficha != null)
+                    {
+                        if (fila == 0) return game;
+                        indiceTope = fila - 1;
+                        break;
+                    }
+                }
+                    if(indiceTope == -1)return game;
+                game.Board.Celdas[indiceTope,indice].Ficha = new Ficha(game.Board.FichaDisparo[desdeDonde][indice].Color);
+                game.Board.FichaDisparo[desdeDonde][indice].Color = Ficha.GetRandomColorFicha();
+                
+            }
+            return game;
+        }
+
+        public Tablero GetTableroDeNivel(int nivel)
+        {
+            if (nivel == 1)
+            {
+                Tablero tablero = Tablero.GenerateBoardFromString(
+                @"XXXXXXXXXX
+                  XXXXXXXXXX
+                  XXXXXXXXXX
+                  XXXXXXXXXX
+                  XXXXXXRXXX
+                  XXXXXXXRXX
+                  XXXXXXXXXX
+                  XXXXXRXXXX
+                  XXRXXXXXXX
+                  XXXXXXXXXX"
+                );
+                return tablero;
+            }
+            throw new Exception("NO se encontró el niviel deseado");
+        }
+        public IList<FichaDisparo> GetFichaDisparo(int size, Posicion desdeDonde)
+        {
+            IList<FichaDisparo> fichasLaterales = new List<FichaDisparo>();
+            for (int indice = 0; indice < size; indice++) 
+            {
+                FichaDisparo ficha = new FichaDisparo(Ficha.GetRandomColorFicha(), desdeDonde,indice);
+                fichasLaterales.Add(ficha);
+            }
+            return fichasLaterales;
+        }
+    }
+}
